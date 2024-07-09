dc2afd8cc6fa6105e6a9c737e0b9d3b8516cf52e
DESKTOP-24E7HA8\jerry <jerrymin1221@naver.com>
2024-07-09 18:19:49 +0900

    김영한님 방식

--- src/main/java/study/data_jpa/entity/BaseTimeEntity.java
+++ src/main/java/study/data_jpa/entity/BaseTimeEntity.java
@@ -1,26 +1,22 @@
 package study.data_jpa.entity;
 
-import jakarta.persistence.*;
+import jakarta.persistence.Column;
+import jakarta.persistence.EntityListeners;
+import jakarta.persistence.MappedSuperclass;
 import lombok.Getter;
-import org.springframework.data.annotation.CreatedBy;
 import org.springframework.data.annotation.CreatedDate;
-import org.springframework.data.annotation.LastModifiedBy;
 import org.springframework.data.annotation.LastModifiedDate;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 import java.time.LocalDateTime;
-
 @EntityListeners(AuditingEntityListener.class)
 @MappedSuperclass
 @Getter
-public class BaseEntity extends BaseTimeEntity{
-
-    @CreatedBy
+public class BaseTimeEntity {
     @Column(updatable = false)
-    private String createdBy;
+    @CreatedDate
+    private LocalDateTime createdDate;
 
-    @LastModifiedBy
-    private String updatedBy;
-
-
+    @LastModifiedDate
+    private LocalDateTime lastModifiedDate;
 }
