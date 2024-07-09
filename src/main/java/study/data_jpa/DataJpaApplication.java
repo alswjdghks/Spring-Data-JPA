fa6096d25b92052ab17148adb1746c8dcc9a99c2
DESKTOP-24E7HA8\jerry <jerrymin1221@naver.com>
2024-07-09 18:17:14 +0900

    Spring Data JPA Auditing

--- src/main/java/study/data_jpa/DataJpaApplication.java
+++ src/main/java/study/data_jpa/DataJpaApplication.java
@@ -2,12 +2,24 @@ package study.data_jpa;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.data.domain.AuditorAware;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
+import java.util.Optional;
+import java.util.UUID;
+
+@EnableJpaAuditing
 @SpringBootApplication
 public class DataJpaApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(DataJpaApplication.class, args);
 	}
+
+	@Bean
+	AuditorAware<String> auditorProvider() {
+		return () -> Optional.of(UUID.randomUUID().toString()); // 랜덤으로 아이디 출력 -> BaseEntity:createdBy,updatedBy
+	} // 실무에서는 세션정보나, 스프링 시큐리티 로그인 정보에서 ID를 받음
 
 }
