b163abc0dcc128d72cc9d07c0392f10ff41fe1e3
DESKTOP-24E7HA8\jerry <jerrymin1221@naver.com>
2024-07-09 18:56:29 +0900

    Web 확장 - 도메인class 컨버터

---
+++ src/main/java/study/data_jpa/api/MemberController.java
@@ -0,0 +1,33 @@
+package study.data_jpa.api;
+
+import jakarta.annotation.PostConstruct;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+import study.data_jpa.entity.Member;
+import study.data_jpa.repository.MemberRepository;
+
+import java.util.Optional;
+
+@RestController
+@RequiredArgsConstructor
+public class MemberController {
+    private final MemberRepository memberRepository;
+
+    @GetMapping("/members/{id}")
+    public String findMember(@PathVariable("id") long id) {
+        Member findMember = memberRepository.findById(id).get();
+        return findMember.getUsername();
+    }
+
+    @GetMapping("/members/{id}")
+    public String findMember(@PathVariable("id") Member member) {
+        return member.getUsername();
+    }
+
+    @PostConstruct
+    public void init() {
+        memberRepository.save(new Member("userA"));
+    }
+}
